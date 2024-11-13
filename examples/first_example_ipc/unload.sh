a6b702c45bf619b5f2dc23476e373d6ffd5b2570
Xenofon Foukas <xefouk@microsoft.com>
2024-11-13 01:32:12 +0000

    Removed sudo requirement

--- examples/first_example_ipc/unload.sh
+++ examples/first_example_ipc/unload.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-sudo -E $JBPF_PATH/out/bin/jbpf_lcm_cli -u -c codeletset_unload_request.yaml
+$JBPF_PATH/out/bin/jbpf_lcm_cli -u -c codeletset_unload_request.yaml
 
 $JBPFP_PATH/pkg/jbpf_protobuf_cli decoder unload -c codeletset_load_request.yaml
--- examples/first_example_standalone/run_app.sh
+++ examples/first_example_standalone/run_app.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sudo LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$JBPF_PATH/out/lib ./example_app
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$JBPF_PATH/out/lib ./example_app
